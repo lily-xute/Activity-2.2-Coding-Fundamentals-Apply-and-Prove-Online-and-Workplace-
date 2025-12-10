954c23071bec71540fd55a2ce2ff12a953b7ff2b
Lily Xute <lily.xute@capgemini.com>
2025-12-10 15:27:02 +0000

    fix comment

--- main.py
+++ main.py
@@ -67,19 +67,23 @@ def main():
 				if id == 0:
 					projects = get_all_projects()
 					for project in projects:
-						output = (f"Project ID: {project[0]}\r\n"
-								f"Name: {project[1]}\r\n"
-								f"Description: {project[2]}\r\n"
-								f"Status: {project[3]}\n"
-								"----------------")
+						output = (
+									f"Project ID: {project[0]}\r\n"
+									f"Name: {project[1]}\r\n"
+									f"Description: {project[2]}\r\n"
+									f"Status: {project[3]}\n"
+									"----------------"
+								)
 						print(output)
 				else:
 					project = get_project(id)
 					if project:
-						output = (f"Project ID: {project[0]}\r\n"
-								f"Name: {project[1]}\r\n"
-								f"Description: {project[2]}\r\n"
-								f"Status: {project[3]}")
+						output = (
+									f"Project ID: {project[0]}\r\n"
+									f"Name: {project[1]}\r\n"
+									f"Description: {project[2]}\r\n"
+									f"Status: {project[3]}"
+								)
 						print(output)
 					else:
 						print("Project not found.")
